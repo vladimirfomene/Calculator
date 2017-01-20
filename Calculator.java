a711fe6405af9ba95d7e022ae52272098176622b
Vladimir Fomene <vladimirfomene@gmail.com>
2017-01-20 17:35:48 +0000

    Add functionality

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,26 @@
 public class Calculator{
 	public static void main(String[] args){
-
 		
+		
+		
+		double result = 0;
+		switch(Integer.parseInt(args[0])){
 
+			case 1:
+				result = add(Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+			    break;
+			default:
+				break;	
 
+        }
+		
+		System.out.println("Your result is " + result);
 
 	}
 
-	
+	public static double add(double a, double b){
+
+		return a + b;
+	}
 
 }
